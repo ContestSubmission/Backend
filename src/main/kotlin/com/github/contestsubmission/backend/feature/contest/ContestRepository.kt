3ed05d0676f0dd9a04c9abe4cea0037c457232fd
Jakob Hofer <simulatan@proton.me>
2023-12-11 17:19:58 +0000

    Generate API Client with OpenAPI (Documentation#23)

--- src/main/kotlin/com/github/contestsubmission/backend/feature/contest/ContestRepository.kt
+++ src/main/kotlin/com/github/contestsubmission/backend/feature/contest/ContestRepository.kt
@@ -1,10 +1,17 @@
 package com.github.contestsubmission.backend.feature.contest
 
 import com.github.contestsubmission.backend.util.db.CRUDRepository
+import com.github.contestsubmission.backend.util.db.transaction
 import jakarta.enterprise.context.ApplicationScoped
 import java.util.UUID
 
 @ApplicationScoped
 class ContestRepository : CRUDRepository<Contest, UUID>() {
 	override val entityClass: Class<Contest> = Contest::class.java
+
+	suspend fun search(term: String): MutableList<Contest>? = sessionFactory.transaction {
+		it.createQuery("SELECT c FROM Contest c WHERE lower(c.name) LIKE lower(:term)", Contest::class.java)
+			.setParameter("term", "%$term%")
+			.resultList
+	}
 }
