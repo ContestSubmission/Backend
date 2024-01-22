9a65925c22e5b91fa78ce7759e1be20713702a3e
SIMULATAN <SIMULATAN@proton.me>
2024-01-22 19:48:58 +0100

    Implement local api client generation

---
+++ docs/api-client/generate_locally.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+repo_root="../../"
+
+api_client_root="$repo_root/build/api-client/"
+[ -d "$api_client_root" ] && rm -rf "$api_client_root"
+mkdir -p "$api_client_root"
+
+# shellcheck disable=SC2016 # $VERSION is a placeholder
+sed 's/$VERSION/0.0.0/g' package-template.json > "$api_client_root/package.json"
+
+generator_cli_jar="openapi-generator-cli.jar"
+# download if not present
+[ ! -f "$generator_cli_jar" ] && wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/7.2.0/openapi-generator-cli-7.2.0.jar -O "$generator_cli_jar"
+
+(cd "$repo_root" && ./gradlew build)
+java -jar "$generator_cli_jar" generate -i "$repo_root/build/openapi/openapi.yaml" -g typescript-fetch -o "$api_client_root" --additional-properties=typescriptThreePlus=true
